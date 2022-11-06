169e3ce6f4d610827eaacddb94a0f6fb47e772a9
AtlantisSeny <xie1298148098@163.com>
2022-11-06 17:39:52 +0800

    第四次作业

---
+++ HW4/Priests and Devils-Action Separation/Assets/Scripts/InterfaceCon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using modelcon;
+namespace interfacecon
+{
+	public interface ISceneController
+	{
+		void LoadResources();
+	}
+	public interface IUserAction
+	{
+		void MoveBoat();
+		void Restart();
+		void MoveRole(RoleModel role);
+	}
+}
